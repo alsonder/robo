df97e2e6aefc78a4e229c01a2585e49924c4f4ae
Navn <team@example.com>
2024-06-08 14:26:47 +0200

    Game1-> board/player -> Json add

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/rest/game/GameController.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/rest/game/GameController.java
@@ -1,46 +1,30 @@
-package dk.dtu.compute.se.pisd.roborally.rest;
-
+package dk.dtu.compute.se.pisd.roborally.rest.game;
+/*
+import dk.dtu.compute.se.pisd.roborally.rest.IBoard;
+import dk.dtu.compute.se.pisd.roborally.rest.Space;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-public class BoardController {
+public class GameController {
     @Autowired
     private IBoard boardService;
 
-    /*@GetMapping("/game1")
+    @GetMapping("/game1")
     public ResponseEntity<List<Space>> getSpacesForGame1() {
         Long gameId = 1L; // This assumes the ID of Game1 is 1
         List<Space> spaces = boardService.findAll();
         return ResponseEntity.ok(spaces);
-    }*/
-    @GetMapping(value = "/game1", produces = MediaType.TEXT_HTML_VALUE)
-    public ResponseEntity<String> getGame1Links() {
-        String htmlContent = "<html><body>" +
-                "<h1>Game 1 Links</h1>" +
-                "<p><a href='game1/board'>Board Details</a></p>" +
-                "<p><a href='game1/player'>Player Details</a></p>" +
-                "</body></html>";
-        return ResponseEntity.ok(htmlContent);
     }
 
-    @GetMapping("/game1/board")
-    public ResponseEntity<List<Space>> getBoard() {
-        List<Space> spaces = boardService.findAll();
-        return ResponseEntity.ok(spaces);
-    }
-
-
-    /*
     @GetMapping(value = "/board")
     public ResponseEntity<List<Space>> getSpace() {
         List<Space> spaces = boardService.findAll();
         return ResponseEntity.ok().body(spaces);
-    }*/
+    }
 
     @PostMapping("/board")
     public ResponseEntity<String > addSpace(@RequestBody Space p) {
@@ -57,3 +41,4 @@ public class BoardController {
         return ResponseEntity.ok().body(p);
     }
 }
+*/
