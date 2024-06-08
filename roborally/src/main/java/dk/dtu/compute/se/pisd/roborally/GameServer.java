612ee6c4b14c24b06e847813b6c8165fc5a9d061
AlSonder <aleksander.soender@gmail.com>
2024-06-08 14:11:38 +0200

    gameserver added

--- roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/GameServer.java
+++ roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/GameServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoborallyApplication {
+public class GameServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RoborallyApplication.class, args);
+		SpringApplication.run(GameServer.class, args);
 	}
 
 }
